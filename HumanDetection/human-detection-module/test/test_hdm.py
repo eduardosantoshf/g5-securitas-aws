835791ea0c556b22e2822aebd75bbc4fc5eb9a8c
bastos-01 <p.bastos112@gmail.com>
2022-10-17 18:41:06 +0100

    test with mocks + minor changes

--- HumanDetection/human-detection-module/test/test_hdm.py
+++ HumanDetection/human-detection-module/test/test_hdm.py
@@ -1,13 +1,46 @@
+import datetime
 import redis
+import sys
+from unittest.mock import Mock
+
+sys.path.append("../")
+sys.path.append("../../camera")
+
+from human_detection import Human_Detection_Worker
+
 
 r = redis.Redis(host='localhost', port=6379)
 
+
 def test_1():
-    assert r.exists("Cameras")
+    assert r.exists("camera_1")
 
 
 def test_2():
-    assert len(r.hgetall("Cameras")) > 0
+    assert len(r.hgetall("camera_1")) > 0
+
+def test_3():
+    mock = Mock()
+    print("ola")
+    print(mock)
+
+    hdw = Human_Detection_Worker(mock, mock, mock, mock)
+
+    print(hdw.create_database_entry("camera_testing", 1, 1, str(datetime.datetime.now())))
+
+    assert r.hget("camera_testing", 1) != None
+    assert r.hget("camera_testing", 2) == None
+
+
+    
+
+    
+
+
+    
+    
+    
+
    
 
     
