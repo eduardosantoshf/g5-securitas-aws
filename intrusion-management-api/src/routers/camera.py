95223b5a8e8f6ab207cc2adc6eddd4eaadb412f5
TheScorpoi <sobral@ua.pt>
2022-11-08 18:23:21 +0000

    add publish code to intrusion api

--- intrusion-management-api/src/routers/camera.py
+++ intrusion-management-api/src/routers/camera.py
@@ -4,15 +4,70 @@ from fastapi.responses import ORJSONResponse
 import boto3
 from botocore.exceptions import NoCredentialsError
 import os
+import kombu
+from kombu import Exchange, Producer
+
 
 router = APIRouter(
     prefix="/cameras",
     tags=['Cameras']
 )
-@router.post("/receive-intrusion-frame", response_model=schemas.Frame)
+
+#! connection to broker
+kombu_connection = os.environ['AMQP_URL'] + ":5672"
+#kombu_connection = "localhost" + ":5672"
+kombu_exchange = "myuser"
+kombu_channel = "mypassword"
+kombu_producer = "request-video-exchange"
+kombu_queue = "request-video-queue"
+
+
+@router.get("/receive-intrusion-frame", response_model=schemas.Frame)
 def receive_intrusion_frame(frame: schemas.Frame):
-    request_video_to_cameras(frame)
+    print("Received frame ", frame)
+    attach_to_message_broker(kombu_connection, kombu_exchange, kombu_channel, kombu_producer, kombu_queue, frame)    
     return frame
+
+def attach_to_message_broker(broker_url, broker_username,
+                                 broker_password, exchange_name, queue_name, frame):
+        # Create Connection String
+        connection_string = f"amqp://{broker_username}:{broker_password}" \
+            f"@{broker_url}/"
+
+        # Kombu Connection
+        kombu_connection = kombu.Connection(
+            connection_string,
+            #ssl=True
+        )
+        kombu_channel = kombu_connection.channel()
+
+        # Kombu Exchange
+        kombu_exchange = Exchange(
+            name=exchange_name,
+            type="direct",
+            delivery_mode=1
+        )
+
+        # Kombu Producer
+        kombu_producer = Producer(
+            exchange=kombu_exchange,
+            channel=kombu_channel
+        )
+
+        # Kombu Queue
+        kombu_queue = kombu.Queue(
+            name=queue_name,
+            exchange=kombu_exchange
+        )
+        kombu_queue.maybe_bind(kombu_connection)
+        kombu_queue.declare()
+        
+        kombu_producer.publish(frame.dict())
+        print(f"Request made to camera {frame.camera_id} with timestamp {frame.timestamp_intrusion}")
+        
+
+
+
 
 #fazer este pedido ao broker
 @router.get("/request-video", status_code=status.HTTP_200_OK)
