391dd991c8f144df41aa8ae3b6d411b5d98e9ea5
eva-pomposo <evabartolomeu@ua.pt>
2022-11-10 02:40:39 +0000

    connect frontend to api, intrusions videos

--- client-ui/src/components/Client/Dashboard/pages/UsersManagement/Intrusions/Intrusions.js
+++ client-ui/src/components/Client/Dashboard/pages/UsersManagement/Intrusions/Intrusions.js
@@ -1,33 +1,51 @@
-import React from 'react';
-import './Intrusions.css';
-import ReactPlayer from 'react-player'
+import React from "react";
+import "./Intrusions.css";
+import ReactPlayer from "react-player";
 
 function Intrusions() {
-
   return (
     <>
       <h2 className="title">Intrusions</h2>
-      <div className='userList' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <div className='player-wrapper'>
-              <ReactPlayer
-              className='react-player fixed-bottom'
-              url= 'peopledetection.mp4'
-              width='50%'
-              height='50%'
-              controls = {true}
-
-              />
-          </div>
-          <div className='player-wrapper'>
-              <ReactPlayer
-              className='react-player fixed-bottom'
-              url= 'peopledetection.mp4'
-              width='50%'
-              height='50%'
-              controls = {true}
-
-              />
-          </div>
+      <div
+        className="userList"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="player-wrapper">
+          <video
+            id="vidObj"
+            width="50%"
+            height="50%"
+            controls
+            loop
+            muted
+            autoplay
+          >
+            <source
+              src="http://localhost:8000/cameras/intrusions-videos"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+        <div className="player-wrapper">
+        <video
+            id="vidObj"
+            width="50%"
+            height="50%"
+            controls
+            loop
+            muted
+            autoplay
+          >
+            <source
+              src="http://localhost:8000/cameras/intrusions-videos"
+              type="video/mp4"
+            />
+          </video>
+        </div>
       </div>
     </>
   );
