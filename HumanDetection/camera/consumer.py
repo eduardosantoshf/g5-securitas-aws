522f1d2bd2ec8798444f6985d052298d9e6cc2d4
TheScorpoi <sobral@ua.pt>
2022-11-12 18:49:19 +0000

    connect to aws broker

--- HumanDetection/camera/consumer.py
+++ HumanDetection/camera/consumer.py
@@ -5,10 +5,7 @@ from kombu.mixins import ConsumerMixin
 import os
 import math
 import requests
-#from moviepy.video.io.VideoFileClip import VideoFileClip
-#from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-#import moviepy.editor as mp
-
+from dotenv import load_dotenv
 
 # Kombu Message Consuming Human_Detection_Worker
 class Cameras_worker(ConsumerMixin):
@@ -102,6 +99,8 @@ class Consumer_video_request:
         connection_string = f"amqp://{broker_username}:{broker_password}" \
             f"@{broker_url}/"
 
+        print("Connecting to RabbitMQ...")
+
         # Kombu Exchange
         self.kombu_exchange = kombu.Exchange(
             name=exchange_name,
@@ -120,7 +119,7 @@ class Consumer_video_request:
         self.kombu_connection = kombu.Connection(
             connection_string,
             heartbeat=4,
-            #ssl=True
+            ssl=True
         )
 
         # Start Human Detection Workers
@@ -135,13 +134,15 @@ class Consumer_video_request:
 
 if __name__ == '__main__':
 
-    # AMQP Variables
-    RABBIT_MQ_URL = os.environ['AMQP_URL'] + ":5672"
-    RABBIT_MQ_USERNAME = "myuser"
-    RABBIT_MQ_PASSWORD = "mypassword"
-    RABBIT_MQ_EXCHANGE_NAME = "request-video-exchange"
-    RABBIT_MQ_QUEUE_NAME = "request-video-queue"
+    load_dotenv()
 
+    # AMQP Variables
+    RABBIT_MQ_URL = os.getenv('RABBIT_MQ_URL')
+    RABBIT_MQ_USERNAME = os.getenv('RABBIT_MQ_USERNAME')
+    RABBIT_MQ_PASSWORD = os.getenv('RABBIT_MQ_PASSWORD')
+    RABBIT_MQ_EXCHANGE_NAME = os.getenv('RABBIT_MQ_EXCHANGE_NAME')
+    RABBIT_MQ_QUEUE_NAME = os.getenv('RABBIT_MQ_QUEUE_NAME')
+    
     # OUTPUT
     OUTPUT_DIR = "intruders"
 
@@ -154,5 +155,7 @@ if __name__ == '__main__':
         exchange_name=RABBIT_MQ_EXCHANGE_NAME,
         queue_name=RABBIT_MQ_QUEUE_NAME
         )
+    
+    
 
     print("End of consumer")
