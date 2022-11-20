0b1d35d18e5c05d44e7f4da661f44a05280fa74e
TheScorpoi <sobral@ua.pt>
2022-11-20 03:25:09 +0000

    try 2.0

--- intrusion-management-api/src/routers/camera.py
+++ intrusion-management-api/src/routers/camera.py
@@ -81,6 +81,7 @@ def receive_video_from_cameras(file: UploadFile):
 
     if not file.filename.endswith(".mp4"):
         return Response(status_code=status.HTTP_400_BAD_REQUEST, content="File must be mp4")
+    
     with open(file.filename, 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
     print("Video " + file.filename + " received.")
