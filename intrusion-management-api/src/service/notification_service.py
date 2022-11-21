36f83b8afb9c96311d95a68c606ffa10a3806ba1
TheScorpoi <sobral@ua.pt>
2022-11-21 22:17:18 +0000

    organize dir

--- intrusion-management-api/src/service/notification_service.py
+++ intrusion-management-api/src/service/notification_service.py
@@ -2,7 +2,7 @@ import requests
 
 def trigger_notification (camera_id):
     try:    
-        print("make a request to the notification api")        
+        print("Request to Notification API")
         return True
     except Exception as e:
         return False
