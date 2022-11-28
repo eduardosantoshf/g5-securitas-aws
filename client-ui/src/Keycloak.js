2cfeea37ea48a22ecdd6ca69351413eb18732102
Diogo Vicente <diogo.vicente@hotmail.com>
2022-11-28 10:22:14 +0000

    updated docker compose

--- client-ui/src/Keycloak.js
+++ client-ui/src/Keycloak.js
@@ -1,8 +1,8 @@
 import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
  url: "https://0.0.0.0:8443",
- realm: "myrealm",
- clientId: "client1",
+ realm: "g5-securitas",
+ clientId: "g5-securitas",
 });
 
-export default keycloak;
+export default keycloak; 
