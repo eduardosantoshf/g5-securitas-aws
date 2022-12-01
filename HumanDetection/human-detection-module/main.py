ade176447f599848fdd3c67fc7ce7b3654593883
Eduardo Santos <eduardosantoshf@gmail.com>
2022-12-01 17:04:26 +0000

    Testing Redis

--- HumanDetection/human-detection-module/main.py
+++ HumanDetection/human-detection-module/main.py
@@ -16,6 +16,7 @@ load_dotenv()
 RABBIT_MQ_USERNAME = os.environ['RABBIT_MQ_USERNAME']
 RABBIT_MQ_PASSWORD = os.environ['RABBIT_MQ_PASSWORD']
 RABBIT_MQ_URL = os.environ['RABBIT_MQ_URL']
+
 RABBIT_MQ_EXCHANGE_NAME = "human-detection-exchange"
 RABBIT_MQ_QUEUE_NAME = "human-detection-queue"
 
