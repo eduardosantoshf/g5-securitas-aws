f5f5affcad374b49619f07d0f82a3b757a1d6070
eva-pomposo <evabartolomeu@ua.pt>
2022-12-18 01:47:14 +0000

    update

--- management-ui/src/Keycloak.js
+++ management-ui/src/Keycloak.js
@@ -1,6 +1,7 @@
 import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
- url: "http://172.18.0.2:8080",
+//  url: "http://172.18.0.2:8080",
+ url: process.env.REACT_APP_KEYCLOAK_URL,
  realm: "g5-securitas",
  clientId: "management-ui",
 });
