a21192b8b87d8bb62388b6d10f7d97db30fb3e5a
TheScorpoi <sobral@ua.pt>
2022-12-18 20:21:15 +0000

    change user_id to str

--- intrusion-management-api/src/routers/intrusion.py
+++ intrusion-management-api/src/routers/intrusion.py
@@ -9,7 +9,7 @@ router = APIRouter(
 )
 
 @router.get("/events-triggered/{user_id}", status_code=status.HTTP_200_OK)
-def get_events_triggered(user_id: int, db: Session = Depends(get_db)):
+def get_events_triggered(user_id: str, db: Session = Depends(get_db)):
     res = intrusion_service.get_events_triggered(db=db, user_id=user_id)
     if res is None or len(res) == 0:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
