3f4914986e3503eed08aa501e011ef42f45f0409
Diogo Vicente <diogo.vicente@hotmail.com>
2022-12-18 03:10:21 +0000

    callback fix

--- sitesManagementAPI/src/main.py
+++ sitesManagementAPI/src/main.py
@@ -2,11 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import Depends, HTTPException, status
-from fastapi_keycloak import OIDCUser
-from jose import ExpiredSignatureError
 
-from sqlalchemy.orm import Session
-import src.db.repositories.properties_crud as properties_crud, src.models.schemas as schemas
 from src.routers import users, alarms, properties, intrusions, cameras
 from src.db.database import get_db
 from src.idp.idp import idp
@@ -37,22 +33,3 @@ app.include_router(intrusions.router)
 @app.get("/sites-man-api")
 def root():
     return RedirectResponse(url='/sites-man-api/docs')
-
-@app.get("/sites-man-api/callback")
-def callback(db: Session = Depends(get_db), user: OIDCUser = Depends(idp.get_current_user(required_roles=['g5-end-users']))):
-
-    try:
-        idp.get_user(user_id=user.sub, query=None)
-    except ExpiredSignatureError:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Signature expired")
-    except: 
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {owner_id} not found")
-
-    prpty = schemas.PropertyCreate(address=f"{user.sub}'address")
-
-    query = properties_crud.create_property(property=prpty, owner_id=user.sub, db=db)
-    
-    if query is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'Property already registred')
-
-    return RedirectResponse(url='http://securitas-lb-1725284772.eu-west-3.elb.amazonaws.com:80/')
